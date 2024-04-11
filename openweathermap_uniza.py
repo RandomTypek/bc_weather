a54ef3bd2dc58294d4fcd23a23451d8df3cedc76
craynac <simonkrajnak388@gmail.com>
2024-04-11 14:13:33 +0200

    adapt openweathermap script for api 2.5

--- openweathermap_uniza.py
+++ openweathermap_uniza.py
@@ -1,24 +1,16 @@
 import requests
-import pandas as pd
 from datetime import datetime
 
 def call_weather_api(lat, lon, api_key):
-    url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={api_key}&units=metric"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print("Failed to fetch data from the API")
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
         return None
-
-def parse_weather_data(data):
-    if data is None:
-        return None
-    df = pd.DataFrame(data['current'])
-    #df = pd.DataFrame(data['minutely'])
-    #df = pd.DataFrame(data['hourly'])
-    #df = pd.DataFrame(data['daily'])
-    return df
 
 def main():
     api_key = 'API_KEY'
@@ -30,19 +22,17 @@ def main():
     # Call the API
     weather_data = call_weather_api(lat, lon, api_key)
 
-    # Parse the response and save it to DataFrame
-    df = parse_weather_data(weather_data)
-
-    if df is not None:
+    if weather_data:
         current_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
         print(f"Weather data fetched successfully at {current_time}.")
-        for index, row in df.iterrows():
-            print(f"Index: {index}")
-            for col in df.columns:
-                print(f"{col}: {row[col]}")
-            print()
+        for key, value in weather_data.items():
+            if isinstance(value, dict):
+                for sub_key, sub_value in value.items():
+                    print(f"{key}.{sub_key}: {sub_value}")
+            else:
+                print(f"{key}: {value}")
     else:
-        print("Failed to fetch weather data.")
+        print("Failed to fetch weather forecast.")
 
 if __name__ == "__main__":
     main()
