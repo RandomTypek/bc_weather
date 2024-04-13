c466e62e72d780a369b5676a08eff0c388849945
craynac <simonkrajnak388@gmail.com>
2024-04-13 10:34:53 +0200

    load stops from csv

--- tool/weathergrab.py
+++ tool/weathergrab.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from datetime import datetime
 
 def call_weather_api(lat, lon, api_key):
@@ -13,26 +14,38 @@ def call_weather_api(lat, lon, api_key):
         return None
 
 def main():
-    api_key = 'API_KEY'
+    api_key = '2bcf118b0f860b69e1adb49dc0022576'
 
-    # Latitude and longitude for the location
-    lat = 49.201359
-    lon = 18.754791
+    with open('../data/stops_small.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        
+        for row in reader:
+            # Extract latitude and longitude from the row
+            lat = float(row['Zemepisna sirka'].replace(',', '.'))
+            lon = float(row['Zemepisna dlzka'].replace(',', '.'))
 
-    # Call the API
-    weather_data = call_weather_api(lat, lon, api_key)
+            # Skip if latitude or longitude is zero
+            if lat == 0 or lon == 0:
+                print(f"Ignoring row for bus stop {row['Nazov zastavky']}: Latitude or longitude is zero.")
+                print()
+                continue
 
-    if weather_data:
-        current_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-        print(f"Weather data fetched successfully at {current_time}.")
-        for key, value in weather_data.items():
-            if isinstance(value, dict):
-                for sub_key, sub_value in value.items():
-                    print(f"{key}.{sub_key}: {sub_value}")
+            # Call the API
+            weather_data = call_weather_api(lat, lon, api_key)
+
+            # Print weather data if fetched successfully
+            if weather_data:
+                current_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+                print(f"Weather data fetched successfully for bus stop {row['Obec']} {row['Nazov zastavky']} at {current_time}.")
+                for key, value in weather_data.items():
+                    if isinstance(value, dict):
+                        for sub_key, sub_value in value.items():
+                            print(f"{key}.{sub_key}: {sub_value}")
+                    else:
+                        print(f"{key}: {value}")
+                print()  # Add an empty line for separation
             else:
-                print(f"{key}: {value}")
-    else:
-        print("Failed to fetch weather forecast.")
+                print(f"Failed to fetch weather forecast for bus stop {row['Nazov zastavky']}.")
 
 if __name__ == "__main__":
     main()
